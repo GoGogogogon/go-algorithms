7601d6a9b7636d2ec6bb0a78153c778c4c9b8ef4
orimono <hayaome123@gmail.com>
2026-04-20 22:10:10 +0900

    A04.goの追加

---
+++ chapter1/A04/A04.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	var n int
+// 	fmt.Scan(&n)
+
+// 	for i := 9; i >= 0; i-- {
+// 		xi := 1 << i
+// 		count := (n / xi) % 2
+// 		fmt.Print(count)
+// 		// 0000000000
+// 	}
+// 	fmt.Println("")
+
+// 	// 1か0かの式を用意し、毎回計算することで実装している
+// }
+
+func main() {
+
+	var n int
+	fmt.Scan(&n)
+	for i := 8; i >= 0; i-- {
+		xi := 1 << i
+
+	}
+}
