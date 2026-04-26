8158827a32e4777fac6f6d7407ffe8bb98612bcd
orimono <hayaome123@gmail.com>
2026-04-26 23:57:39 +0900

    chaper8の追加

--- chapter4/A16/A16.go
+++ chapter4/A16/A16.go
@@ -1,34 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
+
+// func main() {
+// 	var n int
+// 	in := bufio.NewReader(os.Stdin)
+
+// 	fmt.Fscan(in, &n)
+
+// 	dp := make([]int, n+1)
+
+// 	a := make([]int, n+10)
+
+// 	b := make([]int, n+10)
+// 	for i := 2; i <= n; i++ {
+// 		fmt.Fscan(in, &a[i])
+// 	}
+
+// 	for i := 3; i <= n; i++ {
+// 		fmt.Fscan(in, &b)
+// 	}
+// 	dp[1] = 0
+// 	dp[2] = a[2]
+
+// 	for i := 3; i <= n; i++ {
+// 		dp[i] = min(dp[i-1]+a[i], dp[i-2]+b[i])
+// 	}
+
+// 	fmt.Println(dp)
+
+// }
 
 func main() {
 	var n int
-	//in := bufio.NewReader(os.Stdin)
-
-	fmt.Scan(&n)
-
-	dp := make([]int, n+1)
-
-	a := make([]int, n+10)
-
-	b := make([]int, n+10)
-	for i := 2; i <= n; i++ {
-		fmt.Scan(&a[i])
-	}
-
-	for i := 3; i <= n; i++ {
-		fmt.Scan(&b[i])
-	}
-	dp[1] = 0
-	dp[2] = a[2]
-
-	for i := 3; i <= n; i++ {
-		dp[i] = min(dp[i-1]+a[i], dp[i-2]+b[i])
-	}
-
-	fmt.Println(dp[n])
+	in := bufio.NewReader(os.Stdin)
+	fmt.Fscan(in, &n)
 
 }
